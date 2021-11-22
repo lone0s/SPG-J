6f5470da7b2847862b4fea6155bfcbec7c85b7e9
Clement <clement.gelinaud@orange.fr>
2021-11-22 16:42:13 +0100

    Mag + MagTest

--- src/main/java/item/Mag.java
+++ src/main/java/item/Mag.java
@@ -2,10 +2,27 @@ package item;
 
 import character.Player;
 
+import java.util.Random;
+
 public class Mag extends Item{
 
-    Mag(String name, String description) {
-        super(name, description);
+    private static final int MIN_AMMO = 20;
+    private static final int MAX_AMMO = 50;
+
+    private int nbAmmo;
+
+    Mag() {
+        super("Mag", "Utilise les chargeurs pour recharger ton arme !");
+        this.setRandomAmmo();
+    }
+
+    public void setRandomAmmo() {
+        Random nbAleatory = new Random();
+        nbAmmo = MAX_AMMO + nbAleatory.nextInt(MAX_AMMO - MIN_AMMO);
+    }
+
+    public int getNbAmmo() {
+        return this.nbAmmo;
     }
 
     @Override
--- src/test/java/item/MagTest.java
+++ src/test/java/item/MagTest.java
@@ -1,7 +1,27 @@
 package item;
 
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class MagTest {
+
+    private Mag mag;
+
+    @Before
+    public void setUp() {
+        mag = new Mag();
+    }
+
+    @Test
+    public void getNbAmmoTest(){
+        System.out.println("Le chargeur possède " + mag.getNbAmmo() + " munitions.");
+    }
+
+    @Test
+    public void printItemTest() {
+        mag.printItem();
+    }
 
 }
