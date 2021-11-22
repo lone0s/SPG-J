651920d10103ea798a1b4117bb1d9c62b9fe1e73
Clement <clement.gelinaud@orange.fr>
2021-11-22 16:25:28 +0100

    NPC + Test

--- src/main/java/character/NPC.java
+++ src/main/java/character/NPC.java
@@ -2,8 +2,14 @@ package character;
 
 public class NPC extends Character {
 
-
+    // Constructor
     public NPC(String name) {
         super(name);
     }
+
+    // Methode pour afficher les dialogues du NPC
+    public void dialog(String dialog) {
+        System.out.println(this + dialog);
+    }
+
 }
