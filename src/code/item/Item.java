bfc07cb38256df4e569160ad9a962fd1ca1a7d7f
Alessandro <alessandro@mail.fr>
2021-11-22 09:24:11 +0100

    Création dossier test

--- src/code/item/Item.java
+++ src/code/item/Item.java
@@ -7,9 +7,7 @@ public abstract class Item {
     private final String name;
     private final String description;
 
-
-    Item(String name, String description){
-
+    Item(String name, String description) {
         this.name = name;
         this.description = description;
 
@@ -20,9 +18,6 @@ public abstract class Item {
 
 
     public void printItem (){
-
         System.out.println( "- " + this.name + " : " + this.description);
-
     }
-
 }
---
+++ src/test/java/ItemTest.java
@@ -0,0 +1,7 @@
+package java;
+
+import static org.junit.Assert.*;
+
+public class ItemTest {
+
+}
