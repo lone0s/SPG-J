23e4404eb8390b8bf0282af57cc4547c33545d2f
Alessandro <alessandro@mail.fr>
2021-11-22 14:21:31 +0100

    Modifs test Player

--- src/test/java/character/PlayerTest.java
+++ src/test/java/character/PlayerTest.java
@@ -1,7 +1,22 @@
 package character;
 
+import org.junit.Before;
+import org.junit.Test;
+
 import static org.junit.Assert.*;
 
 public class PlayerTest {
+    private Player joueur;
+
+    @Before
+    public void setUp() {
+        joueur = new Player("Joueur 1");
+    }
+
+    @Test
+    public void toStringTest() {
+        String affichage = joueur.toString();
+        System.out.println(affichage);
+    }
 
 }
