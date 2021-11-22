bbbf1df62ef37623c83c7edaa494bd3a600d4293
Alessandro <alessandro@mail.fr>
2021-11-22 14:09:13 +0100

    Ajout test Charactere

---
+++ src/test/java/character/CharacterTest.java
@@ -0,0 +1,17 @@
+package character;
+
+import org.junit.Before;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class CharacterTest {
+
+    @Test
+    public void getName() {
+    }
+
+    @Test
+    public void testToString() {
+    }
+}
