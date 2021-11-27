017e21ad421fdb30c03535e677cdd4ebbd06dae3
alessandro <mail@mail.fr>
2021-11-27 13:07:47 +0100

    Création de Symbol()

--- src/main/java/squid/place/game/TicTacToe/Symbol.java
+++ src/main/java/squid/place/game/TicTacToe/Symbol.java
@@ -3,7 +3,7 @@ package squid.place.game.TicTacToe;
 import java.util.Scanner;
 
 public class Symbol {
-    private final String Symbol;
+    private String Symbol;
 
     /**
      * Choix du symbole du joueur
@@ -11,28 +11,29 @@ public class Symbol {
     Symbol() {
         Scanner scanner = new Scanner(System.in);
         String symbol = "";
+        boolean symbolValid = false;
 
-        while (!checkValiditySymbol(symbol)) {
+        while (!symbolValid) {
             System.out.print("Please choose your symbol (X or O) : ");
 
             if (scanner.hasNext()) {
                 symbol = scanner.next();
             }
             scanner.nextLine();
+            if (checkValiditySymbol(symbol)) {
+                this.Symbol = symbol;
+                symbolValid = true;
+            }
         }
-
-        this.Symbol = symbol;
     }
 
-    /**
-     * @param symbol Symbole à associer à l'ordinateur
-     */
     Symbol(String symbol) {
         if (checkValiditySymbol(symbol)) {
             this.Symbol = symbol;
         }
         else {
-
+            System.out.println("Erreur lors du choix du symbole de TicTacToe de l'ordinateur : le symbole n'est pas valide.");
+            System.exit(-1);
         }
     }
 
