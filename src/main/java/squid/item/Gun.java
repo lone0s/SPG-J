7cb89cb22f35de358749b770b5e23faab8eed795
alessandro <mail@mail.fr>
2021-12-01 19:55:49 +0100

    Gun terminé

--- src/main/java/squid/item/Gun.java
+++ src/main/java/squid/item/Gun.java
@@ -3,8 +3,9 @@ package squid.item;
 import squid.character.Player;
 import squid.place.Place;
 
-public class Gun extends Item {
+import java.util.List;
 
+public class Gun extends Item {
     private final static String name = "Pistolet";
     private final static String description = "Pour utiliser ce pistolet, il faut posséder un chargeur.";
 
@@ -12,18 +13,39 @@ public class Gun extends Item {
      * Ce constructeur créer un pistolet avec une explication pour l'utiliser.
      */
     public Gun() {
-        super(name, description);
+        super("Gun", "To use the gun, you must first have a mug (charged)");
     }
 
     @Override
     public void use(Player player) {
-    }
+        Place place = player.getPlace();
+        List<Item> items = player.getItems();
 
-    /**
-     * Affichage du pistolet
-     */
-    @Override
-    public void printItem() {
-        super.printItem();
+        for (int compteur = 0 ; compteur < items.size() ; compteur++) {
+            if (items.get(compteur).getName().equalsIgnoreCase("Mag")) {
+                Mag mag = (Mag)items.get(compteur);
+                if (mag.getNbAmmo() == -1) {
+                    System.out.println("[*] You have to use first your mag.\n");
+                    break;
+                }
+                else {
+                    place.setNbPlayer(place.getNbPlayer() - mag.getNbAmmo());
+                    System.out.println("[*] You killed " + mag.getNbAmmo() + " people.");
+                    player.removeItem(mag);
+
+                    List<Item> nouveauItems = player.getItems();
+                    for (int compteur2 = 0 ; compteur2 < nouveauItems.size() ; compteur2++) {
+                        if (nouveauItems.get(compteur).getName().equalsIgnoreCase("Gun")) {
+                            player.removeItem(nouveauItems.get(compteur2));
+                            break;
+                        }
+                    }
+                    break;
+                }
+            }
+            else {
+                System.out.println("You cannot use the gun without the mag.");
+            }
+        }
     }
 }
