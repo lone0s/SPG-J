80aca27d00e0614ff20f8f3ad02e846454e0f6e9
Clement <clement.gelinaud@orange.fr>
2021-12-01 19:11:19 +0100

    CommandTest

--- src/test/java/squid/command/CommandTest.java
+++ src/test/java/squid/command/CommandTest.java
@@ -1,7 +1,40 @@
 package squid.command;
 
-import junit.framework.TestCase;
+import org.junit.Before;
+import org.junit.Test;
+import squid.character.Player;
 
-public class CommandTest extends TestCase {
+import static org.junit.Assert.*;
+
+public class CommandTest{
+
+    Command command1;
+    Command command2;
+    Command command3;
+
+    @Before
+    public void setUp(){
+        command1 = new Command("Command1", "Desc Command 1") {
+            @Override
+            public void executeCommand(Player player, String[] args) {
+                System.out.println("Execute command 1");
+            }
+        };
+        command2 = new Command("Command2", "Desc Command 2") {
+            @Override
+            public void executeCommand(Player player, String[] args) {
+                System.out.println("Execute command 2");
+            }
+        };
+        command3 = command1;
+    }
+
+    @Test
+    public void getCommand(){
+        assertEquals(command1.getCommand(), "Command1");
+        assertEquals(command2.getCommand(), "Command2");
+        assertSame(command3.getCommand(), command1.getCommand());
+    }
+
 
 }
