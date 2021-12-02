1af578d36cc8ac48661b3595a3e70d3aeaff8fef
alessandro <mail@mail.fr>
2021-12-02 17:57:52 +0100

    Use terminé

--- src/main/java/squid/command/Use.java
+++ src/main/java/squid/command/Use.java
@@ -4,6 +4,7 @@ import squid.character.Player;
 import squid.item.Gun;
 import squid.item.Item;
 import squid.item.Knife;
+import squid.place.Place;
 
 import java.util.List;
 
@@ -15,55 +16,61 @@ public class Use extends Command{
 
     @Override
     public void executeCommand(Player player, String[] args) {
-        List<Item> items = player.getItems();
+        if (player.getPlace().getName().equalsIgnoreCase("MainRoom")) {
+            List<Item> items = player.getItems();
 
-        if (args[1].equalsIgnoreCase("Knife")) {
-            boolean valid = false;
-            for (int compteur = 0 ; compteur < items.size() ; compteur++) {
-                if (items.get(compteur).getName().equalsIgnoreCase("Knife")) {
-                    Item knife = items.get(compteur);
-                    knife.use(player);
-                    valid = true;
-                    break;
+            if (args[1].equalsIgnoreCase("Knife")) {
+                boolean valid = false;
+                for (int compteur = 0 ; compteur < items.size() ; compteur++) {
+                    if (items.get(compteur).getName().equalsIgnoreCase("Knife")) {
+                        Item knife = items.get(compteur);
+                        knife.use(player);
+                        valid = true;
+                        break;
+                    }
+                }
+                if (!valid) {
+                    System.out.println("[*] You don't have knife.");
                 }
             }
-            if (!valid) {
-                System.out.println("[*] You don't have knife.");
-            }
-        }
 
-        else if (args[1].equalsIgnoreCase("Mag")) {
-            boolean valid = false;
-            for (int compteur = 0 ; compteur < items.size() ; compteur++) {
-                if (items.get(compteur).getName().equalsIgnoreCase("Mag")) {
-                    Item mag = items.get(compteur);
-                    mag.use(player);
-                    valid = true;
-                    break;
+            else if (args[1].equalsIgnoreCase("Mag")) {
+                boolean valid = false;
+                for (int compteur = 0 ; compteur < items.size() ; compteur++) {
+                    if (items.get(compteur).getName().equalsIgnoreCase("Mag")) {
+                        Item mag = items.get(compteur);
+                        mag.use(player);
+                        valid = true;
+                        break;
+                    }
+                }
+                if (!valid) {
+                    System.out.println("[*] You don't have mag.");
                 }
             }
-            if (!valid) {
-                System.out.println("[*] You don't have mag.");
-            }
-        }
 
-        else if (args[1].equalsIgnoreCase("Gun")) {
-            boolean valid = false;
-            for (int compteur = 0 ; compteur < items.size() ; compteur++) {
-                if (items.get(compteur).getName().equalsIgnoreCase("Gun")) {
-                    Item gun = items.get(compteur);
-                    gun.use(player);
-                    valid = true;
-                    break;
+            else if (args[1].equalsIgnoreCase("Gun")) {
+                boolean valid = false;
+                for (int compteur = 0 ; compteur < items.size() ; compteur++) {
+                    if (items.get(compteur).getName().equalsIgnoreCase("Gun")) {
+                        Item gun = items.get(compteur);
+                        gun.use(player);
+                        valid = true;
+                        break;
+                    }
+                }
+                if (!valid) {
+                    System.out.println("[*] You don't have gun.");
                 }
             }
-            if (!valid) {
-                System.out.println("[*] You don't have gun.");
+
+            else {
+                System.out.println("[*] The name of object is incorrect.");
             }
         }
-
         else {
-            System.out.println("[*] The name of object is incorrect.");
+            System.out.println("Vous n'êtes pas dans le hub.");
         }
+
     }
 }
